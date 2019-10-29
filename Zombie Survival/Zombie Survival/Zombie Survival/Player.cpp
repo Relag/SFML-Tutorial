9107b375e8beaaa43e78d5a4cc4271e09d938083
Relag <44652125+Relag@users.noreply.github.com>
2019-10-29 17:29:15 +0000

    Up to page 197

--- Zombie Survival/Zombie Survival/Zombie Survival/Player.cpp
+++ Zombie Survival/Zombie Survival/Zombie Survival/Player.cpp
@@ -43,6 +43,110 @@ bool Player::Hit(Time timeHit) {
 		return true;
 	}
 	else {
-		return false
+		return false;
+	}
+}
+
+FloatRect Player::GetPosition(){
+	return m_Sprite.getGlobalBounds();
+}
+
+Vector2f Player::GetCentre() {
+	return m_Position;
+}
+
+float Player::GetRotation() {
+	return m_Sprite.getRotation();
+}
+
+Sprite Player::GetSprite() {
+	return m_Sprite;
+}
+
+int Player::GetHealth() {
+	return m_Health;
+}
+
+void Player::MoveLeft() {
+	m_LeftPressed = true;
+}
+
+void Player::MoveRight() {
+	m_RightPressed = true;
+}
+
+void Player::MoveUp() {
+	m_UpPressed = true;
+}
+
+void Player::MoveDown() {
+	m_DownPressed = true;
+}
+
+void Player::StopLeft() {
+	m_LeftPressed = false;
+}
+
+void Player::StopRight() {
+	m_RightPressed = false;
+}
+
+void Player::StopUp() {
+	m_UpPressed = false;
+}
+
+void Player::StopDown() {
+	m_DownPressed = false;
+}
+
+void Player::Update(float elapsedTime, Vector2i mousePosition) {
+	if (m_UpPressed) {
+		m_Position.y -= m_Speed * elapsedTime;
+	}
+	if (m_DownPressed) {
+		m_Position.y += m_Speed * elapsedTime;
+	}
+	if (m_RightPressed) {
+		m_Position.x += m_Speed * elapsedTime;
+	}
+	if (m_LeftPressed) {
+		m_Position.x -= m_Speed * elapsedTime;
+	}
+
+	m_Sprite.setPosition(m_Position);
+
+	if (m_Position.x > m_Arena.width - m_TileSize) {
+		m_Position.x = m_Arena.width - m_TileSize;
+	}
+	if (m_Position.x < m_Arena.left + m_TileSize) {
+		m_Position.x = m_Arena.left + m_TileSize;
+	}
+	if (m_Position.y > m_Arena.height - m_TileSize) {
+		m_Position.y = m_Arena.height - m_TileSize;
+	}
+	if (m_Position.y < m_Arena.top - m_TileSize) {
+		m_Position.y = m_Arena.top - m_TileSize;
+	}
+
+	//Calculate the angle the player is facing
+	float angle = (atan2(mousePosition.y - m_Resolution.y / 2,
+		mousePosition.x - m_Resolution.x / 2) * 180) / 3.141;
+
+	m_Sprite.setRotation(angle);
+}
+
+void Player::UpgradeSpeed() {
+	m_Speed += (START_SPEED * .2);
+}
+
+void Player::UpgradeHealth() {
+	m_MaxHealth += (START_HEALTH * .2);
+}
+
+void Player::IncreasedHealthLevel(int amount) {
+	m_Health += amount;
+
+	if (m_Health > m_MaxHealth) {
+		m_Health = m_MaxHealth;
 	}
 }
