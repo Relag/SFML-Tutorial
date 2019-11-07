91e9787480612ae2664f52ab6e80424570b8cf34
Relag <rejean.lagasse93@gmail.com>
2019-11-07 00:32:26 +0000

    Created Texture Holder Class
    Up to page 252/271

---
+++ Zombie Survival/Zombie Survival/TextureHolder.h
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef TEXTURE_HOLDER_H
+#define TEXTURE_HOLDER_H
+
+#include <SFML/Graphics.hpp>
+#include <map>
+
+using namespace std;
+using namespace sf;
+
+class TextureHolder {
+private:
+	//Map container holding related string and texture pairs
+	std::map<std::string, Texture> m_Textures;
+	static TextureHolder* m_s_Instance;
+
+public:
+	TextureHolder();
+	static Texture& GetTexture(string const& filename);
+	
+};
+
+#endif TEXTURE_HOLDER_H
