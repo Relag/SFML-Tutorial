863df0110dd8c66fb2f1de2a9c4344fb7d5c567a
Relag <44652125+Relag@users.noreply.github.com>
2019-11-09 14:11:56 +0000

    up to page 369 (nice)
    created engine.h, added TextureHolder object from Zombie Survival

---
+++ ThomasWasLate/ThomasWasLate/TextureHolder.cpp
@@ -0,0 +1,30 @@
+#include "TextureHolder.h"
+#include <assert.h>
+
+TextureHolder* TextureHolder::m_s_Instance = nullptr;
+
+
+TextureHolder::TextureHolder() {
+	assert(m_s_Instance == nullptr);
+	m_s_Instance = this;
+	m_Textures;
+}
+
+sf::Texture& TextureHolder::GetTexture(std::string const& filename) {
+	//Get Reference to m_Textures using m_s_Instance
+	auto& m = m_s_Instance->m_Textures;
+	
+	auto keyValuePair = m.find(filename);
+
+	if (keyValuePair != m.end()) {
+		//yes, return the texture
+		return keyValuePair->second;
+	}
+	else {
+		//filename not found, create new key value pair
+		auto& texture = m[filename];
+		texture.loadFromFile(filename);
+		return texture;
+	}
+	
+}
