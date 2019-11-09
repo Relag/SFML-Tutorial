9200ac2c6cd553796039a245758ff6b316ea6ad6
Relag <44652125+Relag@users.noreply.github.com>
2019-11-09 15:00:35 +0000

    Created Draw.cpp
    Up to page 361/380

---
+++ ThomasWasLate/ThomasWasLate/Draw.cpp
@@ -0,0 +1,26 @@
+#include "Engine.h"
+
+void Engine::draw() {
+	m_Window.clear(Color::White);
+
+	if (!m_SplitScreen) {
+		//Single-player view
+		m_Window.setView(m_BGMainView);
+		m_Window.draw(m_BackgroundSprite);
+		m_Window.setView(m_MainView);
+	}
+	else {
+		//Splitscreen view
+		m_Window.setView(m_BGLeftView);
+		m_Window.draw(m_BackgroundSprite);
+		m_Window.setView(m_LeftView);
+
+		m_Window.setView(m_BGRightView);
+		m_Window.draw(m_BackgroundSprite);
+		m_Window.setView(m_RightView);
+
+		//Draw hud
+		m_Window.setView(m_HudView);
+		m_Window.display();
+	}
+}
